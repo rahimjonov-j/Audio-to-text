73bd7a5254fee9529e59609c0da96164f033078a
Javohir <javohirrahimjonov546@gmail.com>
2026-03-05 15:18:40 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SpeechToText from './Pagess/Api'
+
+export default function App() {
+  return (
+<>
+<SpeechToText/>
+</>
+  )
+}
---
+++ src/Pagess/Api.jsx
@@ -0,0 +1,106 @@
+import { Mic, MicOff } from "lucide-react";
+import { useRef, useState } from "react";
+
+export default function SpeechToText() {
+  const [recording, setRecording] = useState(false);
+  const [text, setText] = useState("");
+  const mediaRecorderRef = useRef(null);
+  const audioChunks = useRef([]);
+
+  // mic cheker and recording
+  const startRecording = () => {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      const mediaRecorder = new MediaRecorder(stream);
+
+      mediaRecorderRef.current = mediaRecorder;
+      audioChunks.current = [];
+
+      mediaRecorder.ondataavailable = (event) => {
+        audioChunks.current.push(event.data);
+      };
+//  make audio file 
+      mediaRecorder.onstop = () => {
+        const audioBlob = new Blob(audioChunks.current, {
+          type: "audio/webm",
+        });
+
+        sendAudio(audioBlob);
+      };
+
+      mediaRecorder.start();
+      setRecording(true);
+    });
+  };
+// stop recording func
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+    setRecording(false);
+  };
+
+  // sent audio to backend formData
+  const sendAudio = (blob) => {
+    const formData = new FormData();
+
+    formData.append("file", blob);
+    formData.append("language", "uz");
+    formData.append("blocking", "true");
+    formData.append("run_diarization", "false");
+
+    // fetch 
+    fetch("https://uzbekvoice.ai/api/v1/stt", {
+      method: "POST",
+      headers: {
+        Authorization:
+          "6586114f-b21a-4092-9662-9ef54d7e3a07:2c0c7613-189b-4b2c-ad8e-eb9cb7b29c6f",
+      },
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setText(data.result.text || "Text topilmadi");
+      })
+      .catch((err) => {
+        console.error(err);
+        setText("Xatolik yuz berdi");
+      });
+  };
+
+  return (
+    <>
+    <div className="flex w-full justify-center  gap-6 p-10" >
+      <h2 className="font-bold text-[36px]">Ovozni matnga aylantirib beradi</h2>
+    </div>
+    <div className="flex w-full justify-center pt-30 gap-6 p-10">
+
+      {/* mic card */}
+      <div className="w-80 p-6 border rounded-xl shadow  ">
+        <h2 className="text-xl font-bold mb-4">Mikrafoni yoqing!</h2>
+
+        <div className="pt-15">
+          {!recording ? (
+          <button
+            onClick={startRecording}
+            className="bg-green-500 text-white px-3 py-1 rounded"
+          >
+      <Mic />
+          </button>
+        ) : (
+          <button
+            onClick={stopRecording}
+            className="bg-red-500 text-white px-3 py-1 rounded"
+          >
+          <MicOff />
+          </button>
+        )}
+        </div>
+      </div>
+
+      {/* text card */}
+      <div className="w-80 p-6 border rounded-xl shadow">
+        <h2 className="text-xl font-bold mb-4">Matn xolati.</h2>
+        <p>{text || <p className="text-gray-400">Bu yerda natija chiqadi...</p>}</p>
+      </div>
+    </div>
+    </>
+  );
+}
